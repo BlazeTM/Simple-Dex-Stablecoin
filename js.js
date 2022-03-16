3e9b983a800b4f9366d8f676db49f90863037797
BlazeTM <52467136+BlazeTM@users.noreply.github.com>
2022-03-16 18:42:23 +0100

    1
    1

---
+++ js.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".toggleMenu");
+    const navigation = document.querySelector(".navigation");
+    toggleMenu.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
+function darkLight() {
+    const darkLight = document.querySelector(".darkLight");
+    darkLight.classList.toggle("active");
+    var element = document.body;
+    element.classList.toggle("dark-theme");
+}
