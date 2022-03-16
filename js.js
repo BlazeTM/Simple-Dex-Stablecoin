db809640336e3215ac2732df9a13bbe537fb0f78
Kacper Zapart <k4p1email@gmail.com>
2022-03-16 20:52:21 +0100

    123

--- js.js
+++ js.js
@@ -1,12 +1,15 @@
 function toggleMenu() {
     const toggleMenu = document.querySelector(".toggleMenu");
     const navigation = document.querySelector(".navigation");
+    toggleMenu.classList.toggle("active");
+    navigation.classList.toggle("active");
+}
+
+function images() {
     const twitter = document.querySelector(".twitter");
     const discord = document.querySelector(".discord");
     const tiktok = document.querySelector(".tiktok");
     const youtube = document.querySelector(".youtube");
-    toggleMenu.classList.toggle("active");
-    navigation.classList.toggle("active");
     twitter.src='white-twitter.svg';
     discord.src='white-discord-new.svg';
     tiktok.src='white-tiktok.svg';
