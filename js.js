711d89f23fc8677c6c6725ba860e4686360262dc
Kacper Zapart <k4p1email@gmail.com>
2022-03-16 20:16:43 +0100

    1

--- js.js
+++ js.js
@@ -1,8 +1,16 @@
 function toggleMenu() {
     const toggleMenu = document.querySelector(".toggleMenu");
     const navigation = document.querySelector(".navigation");
+    const twitter = document.querySelector("social-twitter")
+    const tiktok = document.querySelector("social-tiktok")
+    const discord = document.querySelector("social-discord")
+    const youtube = document.querySelector("social-youtube")
     toggleMenu.classList.toggle("active");
     navigation.classList.toggle("active");
+    twitter.classList.toggle("active")
+    tiktok.classList.toggle("active")
+    discord.classList.toggle("active")
+    youtube.classList.toggle("active")
 }
 
 function darkLight() {
