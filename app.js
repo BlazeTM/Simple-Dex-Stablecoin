df4c830e0fb8e799a2642102022bee99760a166e
Kacper Zapart <k4p1email@gmail.com>
2022-03-18 14:51:52 +0100

    12132

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
 
 async function getAccount() {
   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
   const account = accounts[0];
   showAccount.innerHTML = account;
+}
+
+function getAdress() {
+  const result = ethereum.isConnected();
 }
