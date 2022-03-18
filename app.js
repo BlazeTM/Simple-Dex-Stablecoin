906e76cbcada7e0a6bcb8769083cc143b8043944
Kacper Zapart <k4p1email@gmail.com>
2022-03-18 14:43:14 +0100

    123123

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
 
-ethereumButton.addEventListener('click', () => {
-  getAccount();
-});
-
 async function getAccount() {
   const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
   const account = accounts[0];
