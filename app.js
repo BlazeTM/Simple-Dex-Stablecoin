ecd440ca7bbc4670035bb3f4a073ee40194f0334
Kacper Zapart <k4p1email@gmail.com>
2022-03-18 15:26:05 +0100

    323

--- app.js
+++ app.js
@@ -5,18 +5,7 @@ const showAccount = document.querySelector('.showAccount');
   const result = ethereum.isConnected();
   if(result == true) {
   const account = accounts;
-  showAccount.innerHTML = account;
+  showAccount.innerText = account;
   };
 }
 
-async function test() {
-  try {
-    const _accounts = await ethereum.request({
-      method: 'eth_accounts',
-    })
-    showAccount.innerHTML = _accounts[0] || 'Not able to get accounts'
-  } catch (err) {
-    console.error(err)
-    showAccount.innerHTML = `Error: ${err.message}`
-  }
-}
