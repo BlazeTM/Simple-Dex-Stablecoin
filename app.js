de3eae0afebca3ee6a0e873b4635481a3ae9daeb
Kacper Zapart <k4p1email@gmail.com>
2022-03-18 15:31:06 +0100

    1212

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-const showAccount = document.querySelector('.showAccount');
+const TIMEOUT = 1000;
+const COLLECTION_NAME = 'DHC';
+let editions = [];
+let dots = 1;
 
- async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const result = ethereum.isConnected();
-  if(result == true) {
-  const account = accounts;
-  showAccount.innerText = account;
+window.addEventListener('DOMContentLoaded', () => {
+  const onboarding = new MetaMaskOnboarding();
+  const onboardButton = document.getElementById('connectWallet');
+  let accounts;
+
+  const updateButton = async () => {
+    if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
+      onboardButton.innerText = 'Install MetaMask!';
+      onboardButton.onclick = () => {
+        onboardButton.innerText = 'Connecting...';
+        onboardButton.disabled = true;
+        onboarding.startOnboarding();
+      };
+    } else if (accounts && accounts.length > 0) {
+      onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+      onboardButton.disabled = true;
+      onboarding.stopOnboarding();
+      checkOwner(accounts[0]);
+    } else {
+      onboardButton.innerText = 'Connect MetaMask!';
+      onboardButton.onclick = async () => {
+        await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        })
+        .then(function(accounts) {
+          onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+          onboardButton.disabled = true;
+          checkOwner(accounts[0]);
+        });
+      };
+    }
   };
-}
+
+  updateButton();
+  if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+    window.ethereum.on('accountsChanged', (newAccounts) => {
+      accounts = newAccounts;
+      updateButton();
+    });
+  }
+});
 
