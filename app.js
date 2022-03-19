30fec9bb8ab2fa42b2202af43ddde9b5b0b6ef8d
Kacper Zapart <k4p1email@gmail.com>
2022-03-19 12:22:25 +0100

    1231

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-const Web3Modal = window.Web3Modal.default;
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
 const providerOptions = {
   /* See Provider Options Section */
-  //walletlink: {
-    /*package: WalletLink, // Required
-    options: {
-      appName: "DHC", // Required
-      infuraId: "acac9625aa514dfebaa00c8735600dff", // Required unless you provide a JSON RPC url; see `rpc` below
-      rpc: "", // Optional if `infuraId` is provided; otherwise it's required
-      chainId: 1, // Optional. It defaults to 1 if not provided
-      appLogoUrl: null, // Optional. Application logo image URL. favicon is used if unspecified
-      darkMode: false // Optional. Use dark theme, defaults to false
-    }
-  },*/
-  walletconnect: {
-    package: WalletConnectProvider, // required
-    options: {
-      infuraId: "acac9625aa514dfebaa00c8735600dff" // required
-    }
-  }
 };
 
 const web3Modal = new Web3Modal({
@@ -26,13 +11,7 @@ const web3Modal = new Web3Modal({
   providerOptions // required
 });
 
-async function onConnect() {
+const instance = await web3Modal.connect();
 
-  console.log("Opening a dialog", web3Modal);
-  try {
-    provider = await web3Modal.connect();
-  } catch(e) {
-    console.log("Could not get a wallet connection", e);
-    return;
-  }
-}
+const provider = new ethers.providers.Web3Provider(instance);
+const signer = provider.getSigner();
